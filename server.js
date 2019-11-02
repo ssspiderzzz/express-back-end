71daf99c3d7c66308e014a5275c826be616187a5
Haibin Zhu <ssspiderzzz@gmail.com>
2019-11-01 17:54:18 -0700

    add dotenv and cors for package.json

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const Express = require('express');
+const Express = require("express");
 const App = Express();
-const BodyParser = require('body-parser');
-const PORT = 8080;
+const BodyParser = require("body-parser");
+const PORT = process.env.PORT || 8080;
+require("dotenv").config();
+const cors = require("cors");
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
-App.use(Express.static('public'));
+App.use(Express.static("public"));
+App.use(cors());
 
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
+App.get("/api/data", (req, res) =>
+  res.json({
+    message: "Seems to work!"
+  })
+);
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(
+    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
+  );
 });
